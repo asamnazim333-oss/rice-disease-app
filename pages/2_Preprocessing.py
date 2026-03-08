d59c966adac821689f05ced57e2cb00b65d40c16
asamnazim333-oss <asamnazim333@gmail.com>
2026-03-08 11:59:13 +0500

    Update 2_Preprocessing.py

--- pages/2_Preprocessing.py
+++ pages/2_Preprocessing.py
@@ -43,3 +43,29 @@ if uploaded_file is not None:
 
     with col3:
         st.image(edges, caption="Edge Detection")
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+st.title("Image Augmentation Preview")
+
+uploaded_file = st.file_uploader("Upload Image")
+
+if uploaded_file:
+
+    image = Image.open(uploaded_file)
+    img = np.array(image)
+
+    img = tf.image.resize(img,(224,224))
+
+    flip = tf.image.flip_left_right(img)
+    rotate = tf.image.rot90(img)
+    bright = tf.image.adjust_brightness(img,0.3)
+
+    col1,col2,col3,col4 = st.columns(4)
+
+    col1.image(img.numpy().astype("uint8"), caption="Original")
+    col2.image(flip.numpy().astype("uint8"), caption="Flip")
+    col3.image(rotate.numpy().astype("uint8"), caption="Rotate")
+    col4.image(bright.numpy().astype("uint8"), caption="Brightness")
