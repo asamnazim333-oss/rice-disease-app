5e94e92a43f5bce779b0038694e366c25b0e4bb2
asamnazim333-oss <asamnazim333@gmail.com>
2026-03-08 11:18:23 +0500

    Update 2_Preprocessing.py

--- pages/2_Preprocessing.py
+++ pages/2_Preprocessing.py
@@ -1,21 +1,45 @@
 import streamlit as st
+import cv2
 import numpy as np
 from PIL import Image
 
-st.title("Image Preprocessing")
+st.title("🧪 Image Preprocessing")
 
-file = st.file_uploader("Upload rice leaf image")
+uploaded_file = st.file_uploader(
+    "Upload Rice Leaf Image",
+    type=["jpg","png","jpeg"]
+)
 
-if file:
+if uploaded_file is not None:
 
-    img = Image.open(file)
+    image = Image.open(uploaded_file)
 
-    st.image(img, caption="Original Image")
+    st.subheader("Original Image")
+    st.image(image)
 
-    img = img.resize((224,224))
+    img = np.array(image)
 
-    st.image(img, caption="Resized Image")
+    # Resize
+    resized = cv2.resize(img, (224,224))
 
-    img = np.array(img)/255.0
+    # Grayscale
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
-    st.write("Normalized shape:",img.shape)
+    # Blur
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+
+    # Edge detection
+    edges = cv2.Canny(blur,50,150)
+
+    st.subheader("Preprocessing Steps")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image(resized, caption="Resized Image")
+
+    with col2:
+        st.image(gray, caption="Grayscale")
+
+    with col3:
+        st.image(edges, caption="Edge Detection")
